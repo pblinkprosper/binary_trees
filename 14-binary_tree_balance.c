851ed95175e73a602bc4c0db7bd7fa3e010f3c13
Prosper <pblinkprosper01@gmail.com>
2023-11-03 01:29:35 +0000

    taask 14 correction

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,19 +9,18 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_l = 0, height_r = 0;
+	size_t l = 0, r = 0;
 
-	if (tree == NULL)
-		return (0);
+	if (tree)
+	{
+		l = (tree->left) ? 1 + binary_tree_height(tree->left) : 1;
 
-	if (tree->left)
-		height_l = 1 + binary_tree_height(tree->left);
-
-	if (tree->right)
-		height_r = 1 + binary_tree_height(tree->right);
-
-	return (height_l > height_r ? height_l : height_r);
+		r = (tree->right) ? 1 + binary_tree_height(tree->right) : 1;
+		return (l > r ? l : r);
+	}
+	return (0);
 }
+
 /**
  * binary_tree_balance - measures the balance factor of a binary tree.
  * @tree: pointer to root node of tree to measure balance factor.
@@ -31,13 +30,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int height_l = 0, height_r = 0;
-
-	if (tree == NULL)
-		return (0);
-
-	height_l = (tree->left) ? (int)binary_tree_height(tree->left) : -1;
-	height_r = (tree->right) ? (int)binary_tree_height(tree->right) : -1;
-
-	return (height_l - height_r);
+	if (tree)
+	{
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	}
+	return (0);
 }
